4b9eae234846e62458ffc8bf5946fc901b921bd8
Ririn Safitri <134044786+Rinsaf14@users.noreply.github.com>
2025-01-18 12:16:45 +0700

    Update route.js

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -3,7 +3,7 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
-  const { id } = params;
+  const { id } = await params;
   const { newTitle: title, newDescription: description } = await request.json();
   await connectMongoDB();
   await Topic.findByIdAndUpdate(id, { title, description });
@@ -11,7 +11,7 @@ export async function PUT(request, { params }) {
 }
 
 export async function GET(request, { params }) {
-  const { id } = params;
+  const { id } = await params;
   await connectMongoDB();
   const topic = await Topic.findOne({ _id: id });
   return NextResponse.json({ topic }, { status: 200 });
