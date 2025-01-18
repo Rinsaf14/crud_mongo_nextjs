2c84bcb0dca4e72d378b73b883161b9394f9abfb
Ririn Safitri <134044786+Rinsaf14@users.noreply.github.com>
2025-01-18 12:29:41 +0700

    Update RemoveBtn.jsx

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -5,18 +5,30 @@ import { useRouter } from "next/navigation";
 
 export default function RemoveBtn({ id }) {
   const router = useRouter();
+
   const removeTopic = async () => {
-    const confirmed = confirm("Are you sure");
+    const confirmed = confirm("Are you sure?");
 
     if (confirmed) {
-      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
-        method: "DELETE",
-      });
-      if (res.ok) {
-        router.refresh();
+      try {
+        const res = await fetch(`/api/topics?id=${id}`, {
+          method: "DELETE",
+        });
+
+        if (res.ok) {
+          // Refresh halaman setelah berhasil menghapus
+          router.refresh();
+        } else {
+          // Tampilkan pesan kesalahan jika penghapusan gagal
+          alert("Failed to delete topic");
+        }
+      } catch (error) {
+        console.log("Error deleting topic:", error);
+        alert("An error occurred while deleting the topic");
       }
     }
   };
+
   return (
     <button onClick={removeTopic} className="text-red-400">
       <HiOutlineTrash size={24} />
